c78277f95871e845be4d683c058db8c0cc644875
Irving-Rafael <70976519+Irving-Rafael@users.noreply.github.com>
2020-10-05 15:32:57 -0500

    Update main.cpp
    Realice un string en el cual ingresas una palabra y te imprime la longitud que tiene esta

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
+#include<string>
+#include<iostream>
 
 using namespace std;
 
-int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
-
+int main()
+{
+    string Longitud;
+    cout<<"Escriba una sola palabra: "<<endl;
+    cin>> Longitud;
+    cout<<Longitud.length()<<endl;
     return 0;
 }
